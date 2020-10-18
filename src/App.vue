b9308003b7184fd69d3a91184fd7c82ccaea0bf2
Aristotele Tufano <aristoteletufano@bitsense.ch>
2020-10-18 19:39:29 +0200

    Working Pikaday input on App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,42 @@
 <template>
   <div class="min-h-screen bg-grey-darker p-8">
-    <div class="max-w-sm mx-auto"></div>
+    <div class="max-w-sm mx-auto">
+      <div class="card">
+        <label class="block">
+          <span class="form-label mb-2">Select a date</span>
+          <input
+            :value="date"
+            class="form-input"
+            type="text"
+            id="datepicker"
+            placeholder="YYYY-MM-DD"
+          />
+        </label>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+/* eslint-disable no-unused-vars */
+import Pikaday from "pikaday"
+
 export default {
-  components: {}
+  data() {
+    return {
+      date: "2020-10-18"
+    }
+  },
+
+  mounted() {
+    const picker = new Pikaday({
+      field: document.getElementById("datepicker"),
+      format: "YYYY-MM-DD",
+      onSelect: () => {
+        this.date = picker.toString()
+      }
+    })
+  }
 }
 </script>
 
