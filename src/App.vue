d2def33e30869872b548118387ee96b132d0bc41
Aristotele Tufano <aristoteletufano@bitsense.ch>
2020-10-19 19:09:20 +0200

    Simple html controlled component select

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,55 @@
 <template>
   <div class="min-h-screen bg-grey-darker p-8">
-    <div class="max-w-sm mx-auto"></div>
+    <div class="max-w-sm mx-auto">
+      <base-select v-model="selectedColor" :options="colors"></base-select>
+
+      <hr />
+    </div>
   </div>
 </template>
 
 <script>
+/* eslint-disable no-unused-vars */
+import $ from "jquery"
+import select2 from "select2"
+import "select2/dist/css/select2.min.css"
+
+import BaseSelect from "./assets/js/components/BaseSelect"
+
 export default {
   name: "App",
 
-  components: {}
+  components: { BaseSelect },
+
+  data() {
+    return {
+      selectedColor: "",
+      colors: [
+        { id: 1, value: "red" },
+        { id: 2, value: "green" },
+        { id: 3, value: "orange" }
+      ]
+    }
+  },
+
+  methods: {
+    triggerSelectInput(event) {
+      // console.log("event", event)
+      // this.selectedColor = event
+    }
+  },
+
+  mounted() {
+    var vm = this
+
+    // $("#select2")
+    //   .select2()
+    //   .on("change", function() {
+    //     // console.log(this.value)
+    //     vm.selectedColor = this.value
+    //     // vm.$emit("input", this.value)
+    //   })
+  }
 }
 </script>
 
---
+++ src/assets/js/components/BaseSelect.vue
@@ -0,0 +1,22 @@
+<template>
+  <select
+    :value="value"
+    @input="$emit('input', $event.target.value)"
+    class="form-input"
+  >
+    <option
+      v-for="option in options"
+      :key="option.id"
+      :value="option.id"
+      v-text="option.value"
+    ></option>
+  </select>
+</template>
+
+<script>
+export default {
+  props: ["value", "options"]
+}
+</script>
+
+<style></style>
