8051e71d96ce8a90fa8771e2022f32bd8eaf8536
Aristotele Tufano <aristoteletufano@bitsense.ch>
2020-10-22 11:03:36 +0200

    Lesson 05 Set up

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,33 @@
 <template>
   <div class="min-h-screen bg-grey-darker p-8">
-    <div class="max-w-sm mx-auto"></div>
+    <div class="max-w-sm mx-auto">
+      <div class="min-h-screen flex items-center justify-center">
+        <button @click="modalOpen = true" type="button" class="btn btn-blue">
+          Open Modal
+        </button>
+      </div>
+
+      <announcement-modal
+        :show="modalOpen"
+        @close="modalOpen = false"
+      ></announcement-modal>
+    </div>
   </div>
 </template>
 
 <script>
+import AnnouncementModal from "./assets/js/components/AnnouncementModal"
+
 export default {
   name: "App",
 
-  components: {}
+  components: { AnnouncementModal },
+
+  data() {
+    return {
+      modalOpen: false
+    }
+  }
 }
 </script>
 
