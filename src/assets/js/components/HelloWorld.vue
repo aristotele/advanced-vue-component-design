9a7b5834a46f29692a8632f4d3bfaccc3f9931fc
Aristotele Tufano <aristoteletufano@bitsense.ch>
2020-10-29 18:20:10 +0100

    Render custom component

--- src/assets/js/components/HelloWorld.vue
+++ src/assets/js/components/HelloWorld.vue
@@ -1,15 +1,24 @@
-<template>
-  <primary-button>
-    Hello World
-  </primary-button>
-</template>
-
 <script>
 import PrimaryButton from "./PrimaryButton"
 
 export default {
   components: {
     PrimaryButton
+  },
+
+  render(createElement) {
+    return createElement(
+      PrimaryButton,
+      {
+        nativeOn: {
+          click: () => alert("clicked!")
+          // click() {
+          //   alert("clicked")
+          // }
+        }
+      },
+      "Hello World"
+    )
   }
 }
 </script>
