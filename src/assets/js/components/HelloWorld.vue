2fdd5e4e0c1a5bc0e1de4ef6393b7fddf369ef17
Aristotele Tufano <aristoteletufano@bitsense.ch>
2020-10-29 18:00:51 +0100

    Basic render function instead of <template>

--- src/assets/js/components/HelloWorld.vue
+++ src/assets/js/components/HelloWorld.vue
@@ -1,9 +1,17 @@
-<template>
-  <span class="text-blue text-xl font-bold">Hello World</span>
-</template>
-
 <script>
-export default {}
+export default {
+  render(createElement) {
+    return createElement(
+      "span",
+      {
+        attrs: {
+          class: "text-blue text-xl font-bold"
+        }
+      },
+      "Hello World"
+    )
+  }
+}
 </script>
 
 <style></style>
