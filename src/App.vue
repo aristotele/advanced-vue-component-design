ae3ef064d0fbddd594904ab69d59d17b89cf971a
Aristotele Tufano <aristoteletufano@bitsense.ch>
2020-10-30 10:54:25 +0100

    Lesson 15 - Set up

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div class="min-h-screen bg-grey-darker p-8">
     <div class="max-w-sm mx-auto bg-white h-screen">
       <div class="flex justify-center pt-8">
-        <hello-world></hello-world>
+        <hello-world>
+          <h1 class="text-blue text-lg font-bold">Hello universe</h1>
+        </hello-world>
       </div>
     </div>
   </div>
--- src/assets/js/components/HelloWorld.vue
+++ src/assets/js/components/HelloWorld.vue
@@ -1,29 +1,11 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
 <script>
-import contacts from "../../../../static/contacts.json"
-
-export default {
-  data() {
-    return { contacts }
-  },
-
-  render(createElement) {
-    // parent wrapper <div> with array of children [<h1>, <ul>]
-    return createElement("div", {}, [
-      createElement("h1", {}, "My ugly header"),
-      createElement(
-        "ul",
-        {},
-        this.contacts.map(contact => {
-          return createElement(
-            "li",
-            {},
-            `${contact.name.first} ${contact.name.last}`
-          )
-        })
-      )
-    ])
-  }
-}
+export default {}
 </script>
 
 <style></style>
