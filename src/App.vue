d4b3d94e115107ba4ef03bbf3a97b77771e1641e
Aristotele Tufano <aristoteletufano@bitsense.ch>
2020-10-30 12:22:43 +0100

    Lesson 16 - Set up

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,20 @@
 <template>
   <div class="min-h-screen bg-grey-darker p-8">
-    <div class="max-w-sm mx-auto bg-white h-screen">
+    <div class="max-w-sm mx-auto">
       <div class="flex justify-center pt-8">
-        <hello-world>
-          <template slot-scope="{ myProperty, anotherProp }">
-            {{ anotherProp }} {{ myProperty }}
-          </template>
-        </hello-world>
+        <contact-list></contact-list>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./assets/js/components/HelloWorld"
+import ContactList from "./assets/js/components/ContactList"
 
 export default {
   name: "App",
 
-  components: { HelloWorld }
+  components: { ContactList }
 }
 </script>
 
