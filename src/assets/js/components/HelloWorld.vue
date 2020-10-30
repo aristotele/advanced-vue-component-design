644a5a0ae6f5a432e99a4b8171adec6cf819a7bb
Aristotele Tufano <aristoteletufano@bitsense.ch>
2020-10-30 10:58:32 +0100

    Reproduce template's slot behavior

--- src/assets/js/components/HelloWorld.vue
+++ src/assets/js/components/HelloWorld.vue
@@ -1,11 +1,17 @@
-<template>
-  <div>
-    <slot></slot>
-  </div>
-</template>
-
 <script>
-export default {}
+export default {
+  render(createElement) {
+    return createElement(
+      "h1",
+      {
+        attrs: {
+          class: "text-lg text-blue font-bold"
+        }
+      },
+      [this.$slots.default]
+    )
+  }
+}
 </script>
 
 <style></style>
