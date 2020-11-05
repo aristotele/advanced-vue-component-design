50f3a13aef608c2de3a8c5dd56293fcdc7a57942
Aristotele Tufano <aristoteletufano@bitsense.ch>
2020-11-05 11:09:12 +0100

    Lesson 26 - Set up

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,52 @@
 <template>
   <div class="min-h-screen bg-grey-darker p-8">
-    <div class="max-w-sm mx-auto"></div>
+    <div class="max-w-sm mx-auto card mt-8">
+      <accordion-list>
+        <accordion-item :item-id="1">
+          <template slot="header">Item A</template>
+          <template slot="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            finibus auctor massa, vitae posuere sem tincidunt eget. Duis non
+            vestibulum diam. Pellentesque eleifend interdum mollis. Donec
+            porttitor dui enim. Maecenas a semper sem. Nunc molestie turpis
+            vitae sem auctor, non venenatis velit ultricies.
+          </template>
+        </accordion-item>
+
+        <accordion-item :item-id="2">
+          <template slot="header">Item B</template>
+          <template slot="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            finibus auctor massa, vitae posuere sem tincidunt eget. Duis non
+            vestibulum diam. Pellentesque eleifend interdum mollis. Donec
+            porttitor dui enim. Maecenas a semper sem. Nunc molestie turpis
+            vitae sem auctor, non venenatis velit ultricies.
+          </template>
+        </accordion-item>
+
+        <accordion-item :item-id="3">
+          <template slot="header">Item C</template>
+          <template slot="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            finibus auctor massa, vitae posuere sem tincidunt eget. Duis non
+            vestibulum diam. Pellentesque eleifend interdum mollis. Donec
+            porttitor dui enim. Maecenas a semper sem. Nunc molestie turpis
+            vitae sem auctor, non venenatis velit ultricies.
+          </template>
+        </accordion-item>
+      </accordion-list>
+    </div>
   </div>
 </template>
 
 <script>
+import AccordionList from "./assets/js/components/AccordionList"
+import AccordionItem from "./assets/js/components/AccordionItem"
+
 export default {
   name: "App",
 
-  components: {}
+  components: { AccordionList, AccordionItem }
 }
 </script>
 
---
+++ src/assets/js/components/AccordionList.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="accordion-list">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
