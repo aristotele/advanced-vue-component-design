972dff8ea623dbcc9da20a126723937e9a0d2f00
Aristotele Tufano <aristoteletufano@bitsense.ch>
2020-11-06 09:51:00 +0100

    Lesson 28 - Set up

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="min-h-screen bg-grey-darker p-8">
-    <div class="max-w-sm mx-auto card mt-8"></div>
+    <div class="max-w-sm mx-auto card mt-8">
+      <div class="mb-4">
+        <label class="form-label mb-2">Favorite Thrash Band</label>
+        <search-select></search-select>
+      </div>
+      <div class="text-right">
+        <button type="button" class="btn btn-blue">Save Changes</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import SearchSelect from "./assets/js/components/SearchSelect"
+
 export default {
   name: "App",
 
-  components: {}
+  components: { SearchSelect }
 }
 </script>
 
